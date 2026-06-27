e053536adaffcdb1fa0eccb0e13ead366cf062bf
Leonardo Lima <eng.leolsouza@hotmail.com>
2026-06-27 11:35:42 -0300

    fix: add google book key in instance

--- lib/google-books-instance.ts
+++ lib/google-books-instance.ts
@@ -2,6 +2,9 @@ import axios from "axios"
 
 const googleBooks = axios.create({
   baseURL: "https://www.googleapis.com/books/v1",
+  params: {
+    key: process.env.GOOGLE_BOOKS_API_KEY,
+  },
 })
 
 export default googleBooks
